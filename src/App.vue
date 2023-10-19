4492c4b36973e5120975fec79b92a25eb385b692
Matija Kočevar <matija.kocev@gmail.com>
2023-10-19 21:22:15 +0200

    Refactor App component to use NavigationBar component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,35 @@
 <template>
-  <div id="app">
-    <p>
-      <router-link to="/">
-        Registration
-      </router-link>
-      <router-link to="/about">
-        About
-      </router-link>
-    </p>
+  <NavigationBar @navbar-resized="handleNavbarResize" />
+
+  <div class="container views" :style="contentStyles">
     <router-view />
   </div>
 </template>
+
+<script>
+  import NavigationBar from './components/NavigationBar/NavigationBar.vue'
+
+  export default {
+    components: {
+      NavigationBar
+    },
+    data() {
+      return {
+        navbarHeight: 0
+      }
+    },
+    computed: {
+      contentStyles() {
+        const paddingTop = this.navbarHeight + 'px'
+        return { paddingTop }
+      }
+    },
+    methods: {
+      handleNavbarResize(navbarHeight) {
+        this.navbarHeight = navbarHeight
+      }
+    }
+  }
+</script>
+
+<style src="./AppStyle.scss" scoped />
