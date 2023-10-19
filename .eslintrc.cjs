bff3f9978658c5e0346e0bf5361cdfbd9997861a
Matija Kočevar <matija.kocev@gmail.com>
2023-10-19 21:22:28 +0200

    Update EsLint and Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,12 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:vue/vue3-recommended",
-  ],
+  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended'],
   rules: {
-  },
-};
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/max-attributes-per-line': 'off'
+  }
+}
