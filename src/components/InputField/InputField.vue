372911e80b626af45f26d33b97e07fdde4f32d88
Matija Kočevar <matija.kocev@gmail.com>
2023-10-20 04:03:39 +0200

    Add InputField component

---
+++ src/components/InputField/InputField.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="input-field">
+    <label :for="id">{{ label }}:</label>
+    <input :id="id" ref="inputField" :value="localValue" :type="type" :required="isRequired" @input="handleInput" />
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      id: {
+        type: String,
+        required: true
+      },
+      label: {
+        type: String,
+        required: true
+      },
+      type: {
+        type: String,
+        required: true
+      },
+      value: {
+        type: String,
+        default: ''
+      },
+      errorMessage: {
+        type: String,
+        required: true
+      },
+      isRequired: Boolean
+    },
+    emits: ['input'],
+    data() {
+      return {
+        localValue: this.value
+      }
+    },
+    methods: {
+      handleInput() {
+        this.localValue = this.$refs.inputField.value
+        this.$emit('input', this.localValue)
+        this.validateInput()
+      },
+      validateInput() {
+        const input = this.$refs.inputField
+
+        if (input.value.trim() === '' && this.isRequired) {
+          input.setCustomValidity(this.errorMessage)
+        } else {
+          input.setCustomValidity('')
+        }
+
+        input.checkValidity()
+        input.reportValidity()
+      }
+    }
+  }
+</script>
+
+<style src="./InputFieldStyle.scss" scoped />
