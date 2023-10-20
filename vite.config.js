3fac92492a993c1864bfbd5e1b959c05109f6586
Matija Kočevar <matija.kocev@gmail.com>
2023-10-20 23:14:31 +0200

    Setup gh-pages, https in development

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import mkcert from "vite-plugin-mkcert";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  base: "/vue-Js-frodx/",
+  plugins: [vue(), mkcert()],
+  server: {
+    host: true,
+    https: true,
+    cors: true,
+  },
 })
