fb18b86290ab4d4362ea4e77d470e9c5935e467b
Matija Kočevar <matija.kocev@gmail.com>
2023-10-21 04:00:17 +0200

    Add RegistrationStatus component

---
+++ src/components/RegistrationStatus/RegistrationStatus.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import { computed } from 'vue'
+  import { useUserRegistrationStore } from '../../stores/userRegistration'
+
+  const store = useUserRegistrationStore()
+  const registrationStatus = computed(() => store.registrationStatus)
+  const errorMessage = computed(() => store.errorMessage)
+</script>
+
+<template>
+  <div>
+    <div v-if="registrationStatus === 'Submitted'">Submitted</div>
+    <div v-else-if="registrationStatus === 'Error'">Error: {{ errorMessage }}</div>
+  </div>
+</template>
