1ecc0ae51a8584d3f93dd483ae60152d39a1ff25
Matija Kočevar <matija.kocev@gmail.com>
2023-10-21 23:07:24 +0200

    Add SampleForm component that uses DynamicForm

---
+++ src/components/SampleForm/SampleForm.vue
@@ -0,0 +1,42 @@
+<script setup>
+  import { ref } from 'vue'
+  import DynamicForm from '../DynamicForm/DynamicForm.vue'
+
+  const formData = ref({
+    name: '',
+    email: ''
+  })
+
+  const handleFormSubmit = (formData) => {
+    console.log('Submitted data:', formData)
+  }
+</script>
+
+<template>
+  <DynamicForm v-model="formData" @submit="handleFormSubmit">
+    <template #default="{ modelValue, input }">
+      <div class="input-field">
+        <label for="name">Name:</label>
+        <input
+          id="name"
+          :modelValue="modelValue.name"
+          type="text"
+          required
+          @input="(e) => input('name', e.target.value)"
+        />
+      </div>
+      <div class="input-field">
+        <label for="email">Email:</label>
+        <input
+          id="email"
+          :modelValue="modelValue.email"
+          type="email"
+          required
+          @input="(e) => input('email', e.target.value)"
+        />
+      </div>
+    </template>
+  </DynamicForm>
+</template>
+
+<style lang="scss" src="./SampleFormStyle.scss" scoped />
--- src/views/FormsPage/FormsPage.vue
+++ src/views/FormsPage/FormsPage.vue
@@ -1,7 +1,11 @@
-<script setup></script>
+<script setup>
+  import SampleForm from '../../components/SampleForm/SampleForm.vue'
+</script>
 
 <template>
-  <div class="forms-page"></div>
+  <div class="forms-page">
+    <SampleForm />
+  </div>
 </template>
 
 <style lang="scss" src="./FormsPageStyle.scss" scoped></style>
