81d24b2d01b83495dc3a1467f58ccdcc96deddd2
Matija Kočevar <matija.kocev@gmail.com>
2023-10-21 23:08:07 +0200

    Add AddressForm component that uses DynamicForm

---
+++ src/components/AddressForm/AddressForm.vue
@@ -0,0 +1,47 @@
+<script setup>
+  import { ref } from 'vue'
+  import DynamicForm from '../DynamicForm/DynamicForm.vue'
+
+  const formData = ref({
+    street: '',
+    city: '',
+    postalCode: ''
+  })
+
+  const handleFormSubmit = (formData) => {
+    console.log('Submitted address:', formData)
+  }
+</script>
+
+<template>
+  <DynamicForm v-model="formData" @submit="handleFormSubmit">
+    <template #default="{ modelValue, input }">
+      <div class="input-field">
+        <label for="street">Street:</label>
+        <input
+          id="street"
+          :value="modelValue.street"
+          type="text"
+          required
+          @input="(e) => input('street', e.target.value)"
+        />
+      </div>
+      <div class="input-field">
+        <label for="city">City:</label>
+        <input id="city" :value="modelValue.city" type="text" required @input="(e) => input('city', e.target.value)" />
+      </div>
+      <div class="input-field">
+        <label for="postalCode">Postal Code:</label>
+        <input
+          id="postalCode"
+          :value="modelValue.postalCode"
+          type="text"
+          required
+          @input="(e) => input('postalCode', e.target.value)"
+        />
+      </div>
+    </template>
+  </DynamicForm>
+</template>
+
+<style lang="scss" src="./AddressFormStyle.scss" scoped />
--- src/views/FormsPage/FormsPage.vue
+++ src/views/FormsPage/FormsPage.vue
@@ -1,10 +1,12 @@
 <script setup>
   import SampleForm from '../../components/SampleForm/SampleForm.vue'
+  import AddressForm from '../../components/AddressForm/AddressForm.vue'
 </script>
 
 <template>
   <div class="forms-page">
     <SampleForm />
+    <AddressForm />
   </div>
 </template>
 
