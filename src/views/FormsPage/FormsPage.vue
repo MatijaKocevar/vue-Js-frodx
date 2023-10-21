8ab432d210b889ea76834a002c563ba2244983fe
Matija Kočevar <matija.kocev@gmail.com>
2023-10-21 23:08:38 +0200

    Add PaymentDetailsForm component that uses DynamicForm

--- src/views/FormsPage/FormsPage.vue
+++ src/views/FormsPage/FormsPage.vue
@@ -1,12 +1,14 @@
 <script setup>
   import SampleForm from '../../components/SampleForm/SampleForm.vue'
   import AddressForm from '../../components/AddressForm/AddressForm.vue'
+  import PaymentDetailsForm from '../../components/PaymentDetailsForm/PaymentDetailsForm.vue'
 </script>
 
 <template>
   <div class="forms-page">
     <SampleForm />
     <AddressForm />
+    <PaymentDetailsForm />
   </div>
 </template>
 
