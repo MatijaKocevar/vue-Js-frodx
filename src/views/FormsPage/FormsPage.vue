1d0719ff662ab63170b34ca4016c4adae230e374
Matija Kočevar <matija.kocev@gmail.com>
2023-10-21 23:09:02 +0200

    Add UserProfileForm component that uses DynamicForm

--- src/views/FormsPage/FormsPage.vue
+++ src/views/FormsPage/FormsPage.vue
@@ -2,6 +2,7 @@
   import SampleForm from '../../components/SampleForm/SampleForm.vue'
   import AddressForm from '../../components/AddressForm/AddressForm.vue'
   import PaymentDetailsForm from '../../components/PaymentDetailsForm/PaymentDetailsForm.vue'
+  import UserProfileForm from '../../components/UserProfileForm/UserProfileForm.vue'
 </script>
 
 <template>
@@ -9,6 +10,7 @@
     <SampleForm />
     <AddressForm />
     <PaymentDetailsForm />
+    <UserProfileForm />
   </div>
 </template>
 
