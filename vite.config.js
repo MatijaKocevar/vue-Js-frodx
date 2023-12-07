9b5f18a422ae28d7b071e0fea1103b33a6c818af
Matija Kočevar <matija.kocev@gmail.com>
2023-12-07 20:28:07 +0100

    Ranamed project

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
-  base: "/vue-Js-frodx/",
+  base: "/vue-dynamic-forms/",
   plugins: [vue(), mkcert()],
   server: {
     host: true,
